58dce57f50ca647d1e61e8754aa8a1f60977200e
md-yusuf-f <mohammedyusuf1799@gmail.com>
2023-12-17 11:02:01 +0530

    Spring to develop a simple JDBC client

---
+++ consuming-RESTful-web-service/src/test/java/com/saf/consumingRESTfulwebservice/ConsumingResTfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saf.consumingRESTfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingResTfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ jdbc-with-spring/src/test/java/com/saf/jdbcwithspring/JdbcWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saf.jdbcwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
